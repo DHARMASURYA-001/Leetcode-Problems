3a9be93dd40ed1e8b5055aba0d037112abbc06ce
DHARMASURYA-001 <suryapavya001@gmail.com>
2024-06-07 10:44:58 +0530

    Added Names of the program

--- 50. Pow(x, n).py
+++ 50. Pow(x, n).py
@@ -1,4 +1,7 @@
-""" Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+""" 
+50. Pow(x, n)
+
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 
 Example 1:
 
